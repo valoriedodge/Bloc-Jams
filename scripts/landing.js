3f6200dc0a40c909444896945007bdbd19945e01
valoriedodge <valoriejones@gmail.com>
2016-07-13 20:40:23 -0700

    asssignment 23 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,27 @@
 var pointsArray = document.getElementsByClassName('point');
  
-var animatePoints = function(points) {
+//var animatePoints = function(points) {
     
- var revealPoint = function(pointArray){
-     for(var i = 0; i<pointArray.length; i++){
-         pointArray[i].style.opacity = 1;
-         pointArray[i].style.transform = "scaleX(1) translateY(0)";
-         pointArray[i].style.msTransform = "scaleX(1) translateY(0)";
-         pointArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
- }
+var revealPoint = function(pointArray){
+     pointArray.style.opacity = 1;
+     pointArray.style.transform = "scaleX(1) translateY(0)";
+     pointArray.style.msTransform = "scaleX(1) translateY(0)";
+     pointArray.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
 
- revealPoint(points);
+// revealPoint(points);
  
- };
+//};
 
 window.onload = function() {
     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+         forEach(pointsArray, revealPoint);
      }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+             forEach(pointsArray, revealPoint);   
          }
      });
  }
