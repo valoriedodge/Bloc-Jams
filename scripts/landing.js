ba017b6bdd977eb7891e0a93e862afa39c95c772
valoriedodge <valoriejones@gmail.com>
2016-07-16 21:30:12 -0700

    checkpoint 29 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,29 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
  
-var animatePoints = function(points) {
-    
- var revealPoint = function(pointArray){
-     for(var i = 0; i<pointArray.length; i++){
-         pointArray[i].style.opacity = 1;
-         pointArray[i].style.transform = "scaleX(1) translateY(0)";
-         pointArray[i].style.msTransform = "scaleX(1) translateY(0)";
-         pointArray[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
- }
-
- revealPoint(points);
- 
+var animatePoints = function() {
+ var revealPoint = function() {
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
  };
+    
+ $.each($('.point'), revealPoint);
+};
 
-window.onload = function() {
-    if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+         animatePoints();
      }
-    var sellingPoints = document.getElementsByClassName('selling-points')[0];
-    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-    window.addEventListener('scroll', function(event) {
-        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+    $(window).scroll(function(event) {
+            if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints(); 
          }
      });
- }
+ });
